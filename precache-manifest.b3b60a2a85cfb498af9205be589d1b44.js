c495d2d7fd19064b3dd2398bbe0d2c72234488cb
Chahat Bhatia <chahatbhatia90@gmail.com>
2020-04-04 17:54:42 +0530

    Updates

---
+++ precache-manifest.b3b60a2a85cfb498af9205be589d1b44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1af292ed5cc082571238da6f0ad9138",
+    "url": "/onTimeFood/index.html"
+  },
+  {
+    "revision": "3855d279cdad5f9a1a98",
+    "url": "/onTimeFood/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b434ae611a0af56e5645",
+    "url": "/onTimeFood/static/css/main.0f13c0a6.chunk.css"
+  },
+  {
+    "revision": "3855d279cdad5f9a1a98",
+    "url": "/onTimeFood/static/js/2.74881e0b.chunk.js"
+  },
+  {
+    "revision": "f4d68181e8c08d94733ca24fb8b98d54",
+    "url": "/onTimeFood/static/js/2.74881e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b434ae611a0af56e5645",
+    "url": "/onTimeFood/static/js/main.98e42b1f.chunk.js"
+  },
+  {
+    "revision": "b905bfda697f60ed7db7",
+    "url": "/onTimeFood/static/js/runtime-main.ee181199.js"
+  },
+  {
+    "revision": "72e68005f0b246b50284ae3f49d22ebf",
+    "url": "/onTimeFood/static/media/logo.72e68005.png"
+  },
+  {
+    "revision": "aa89c7e094f4f3db2e9e9e24ed76d85e",
+    "url": "/onTimeFood/static/media/messy-pizza-on-a-black-table.aa89c7e0.jpg"
+  }
+]);
